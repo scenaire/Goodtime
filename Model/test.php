4d45e5b9b15d583c321dc012ffb59cc6bffba8bb
scenaire <tempesta-psyzeoul@hotmail.com>
2018-04-19 21:09:41 +0700

    เสร็จ product กับ users

--- Model/test.php
+++ Model/test.php
@@ -3,20 +3,26 @@
 require "cart.php";
 require "order.php";
 require "user.php";
-require "userdb.php";
+require "product.php";
 
 //$user = new user("Tempesta","kwankang","จิรัชญา","ยี่โต๊ะ","36/20 ถนนมลิวรรณ ตำบล กุดป่อง อำเภอ เมือง จังหวัด เลย 42000","tempesta-psyzeoul@Hotmail.com");
-$user = new user;
-$user->login("tempesta","kwankang");
-echo $user->getUsername();
-echo "<br><br>";
-echo $user->getEmail();
+//$user = new user;
+//$user->login("tempesta","kwankang");
+//echo $user->getUsername();
+//echo "<br><br>";
+//echo $user->getEmail();
 //echo $user->getCustomerbyUsername("Tempesta");
 
-//$obj = new action;
-//$obj->addProduct("Nendoroid Saitama",1400,array("Product_image\Saitama01.jpg","Product_image\Saitama02.jpg"),"Nendoroid",10,"โล้นซ่า");
-//$obj->addProduct("Nendoroid Midoriya",1690,array("Product_image\Midoriya01.jpg","Product_image\Midoriya02.jpg","Product_image\Midoriya03.jpg"),"Nendoroid",10,"เดกุคุง");
-//$obj->addProduct("Funko Pop Newt Scamender 10",690,array("Product_image/FunkoNewt01.jpg"),"Funko",10,"นิวท์");
+$obj = new product;
+//$obj->addItem("Nendoroid Saitama",1400,array("Product_image\Saitama01.jpg","Product_image\Saitama02.jpg"),"Nendoroid",10,"โล้นซ่า");
+//$obj->addItem("Nendoroid Midoriya",1690,array("Product_image\Midoriya01.jpg","Product_image\Midoriya02.jpg","Product_image\Midoriya03.jpg"),"Nendoroid",10,"เดกุคุง");
+//$obj->addItem("Funko Pop Newt Scamender 10",690,array("Product_image/FunkoNewt01.jpg"),"Funko",10,"นิวท์");
+$obj->selectProduct(34);
+echo $obj->getStock();
+echo "<br><br>";
+echo $obj->setStock(20);
+echo $obj->getStock();
+
 //$obj->getProductFromCategory("Nendoroid");
 //$obj = new product;
 //print_r($obj->getProductFromCategory("Nendoroid"));
